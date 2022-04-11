9251749040f0ec16f0b50ce137233250feb740af
Juan Rios <Juan Rios@JuanRios-Ultron.lan1>
2022-04-10 22:49:08 -0500

    First Commit

---
+++ RLLabs Magic/src/main/java/rllabs/rllabs/magic/RLLabsMagic.java
@@ -0,0 +1,28 @@
+package rllabs.rllabs.magic;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RLLabsMagic implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final Item WAND = new Item(new Item.Settings().group(ItemGroup.MISC));
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+		Registry.register(Registry.ITEM, new Identifier("rllabsmagic", "wand"), WAND);
+	}
+}
